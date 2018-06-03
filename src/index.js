6aec7e8dac1636ec042c67eef1f51954fdf3dc1d
John Livingston <jmlivingston@gmail.com>
2018-06-02 18:10:29 -0700

    Refactor for new style guide

--- src/index.js
+++ src/index.js
@@ -1,7 +1,21 @@
-import React from 'react'
+import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 
-import App from './components/Global/App'
 import './index.scss'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+class Dynamic extends Component {
+  state = {
+    module: null
+  }
+  componentDidMount() {
+    import(`${
+      process.env.REACT_APP_TYPE === 'STYLE_GUIDE' ? './style-guide/StyleGuideApp' : './components/Global/App'
+    }`).then(module => this.setState({ module: module.default }))
+  }
+  render() {
+    const Component = this.state.module
+    return <div>{Component && <Component />}</div>
+  }
+}
+
+ReactDOM.render(<Dynamic />, document.getElementById('root'))
---
+++ src/style-guide/StyleGuideApp.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import { GlobalContainer } from 'components/Global/GlobalContainer'
+import AlertContent from 'style-guide/components/Alert/AlertContent'
+
+const StyleGuideApp = () => {
+  return (
+    <GlobalContainer>
+      <AlertContent />
+    </GlobalContainer>
+  )
+}
+
+export default StyleGuideApp
